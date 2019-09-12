bb5c3e973f0c2c592b48ef4123799771ee55e1a9
Clayton Machado Ilibio <32619055+claytonmi@users.noreply.github.com>
2019-09-12 17:58:13 -0300

    Update hide.js

--- hide.js
+++ hide.js
@@ -1,3 +1,2 @@
 document.querySelector('a').style.display = 'none'
-document.querySelector('.site-footer-credits').style.display = 'none';
-document.querySelector('.site-footer-owner').style.display = 'none'
+
