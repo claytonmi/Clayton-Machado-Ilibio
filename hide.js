85363ea7e185da9192f7176318a38a2330cbdd05
Clayton Machado Ilibio <32619055+claytonmi@users.noreply.github.com>
2019-09-12 18:03:51 -0300

    Update hide.js

--- hide.js
+++ hide.js
@@ -1,2 +1,19 @@
-document.querySelector('a').style.display = 'none'
+function ocultar(id){
+   if( document.querySelector(id)){
+        document.querySelector(id).style.display = 'none'; 
+   }    
+    return  document.querySelector(id).style.display == 'none';
+}
+function servOculta(id){
+  setTimeout(function(){  
+      var ocultou = ocultar(id)
+      if( !ocultou ){
+        servOculta(id);
+      }
+         
+    } , 1);
+}
 
+servOculta('a');
+servOculta('.site-footer-owner');
+servOculta('.site-footer-credits');
